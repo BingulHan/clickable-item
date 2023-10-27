736615d7cf33a07d2d8d77c384f1b15458f5b6ce
bingulhan <bingulhan2005@gmail.com>
2023-10-27 21:30:44 +0300

    Uploaded all files

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-# clickable-item
 
 > An object that moves with your mouse when you click on it
-
-[![NPM](https://img.shields.io/npm/v/clickable-item.svg)](https://www.npmjs.com/package/clickable-item) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-
-## Install
-
-```bash
-npm install --save clickable-item
-```
 
 ## Usage
 
 ```jsx
 import React, { Component } from 'react'
 
-import MyComponent from 'clickable-item'
+import ClickableItemComponent from 'clickable-item'
 import 'clickable-item/dist/index.css'
 
 class Example extends Component {
   render() {
-    return <MyComponent />
+    return <ClickableItemComponent ><p>Test Item</p></ClickableItemComponent>
   }
 }
 ```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useState } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ClickableItemComponent = ({ children }) => {
+
+  const [onClicked, setClicked] = useState(false);
+
+  function onClickItem() {
+    setClicked(!onClicked);
+  }
+
+  const [componentVariable, setComponentVariable] = useState({ left: 0, vars: 0 });
+
+  const mouseMoveHandler = (event) => {
+    if (onClicked == true) {
+      let x = event.clientX;
+      let y = event.clientY;
+      setComponentVariable({ left: x, top: y });
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseMoveHandler)
+    return () => window.removeEventListener('mousemove', mouseMoveHandler);
+  })
+
+  return <div onClick={(e) => { onClickItem() }} className='absolute' style={{ left: componentVariable.left, top: componentVariable.top }}>
+    {children}
+  </div>
 }
