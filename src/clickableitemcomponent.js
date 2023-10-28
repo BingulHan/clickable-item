8daac1e4b9892226e3cb3695923f3a0e2475e8f7
bingulhan <bingulhan2005@gmail.com>
2023-10-28 21:16:50 +0300

    Update index.js

--- src/clickableitemcomponent.js
+++ src/clickableitemcomponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export const ClickableItemComponent = ({ children }) => {
+export default function ClickableItemComponent({ children }) {
 
   const [onClicked, setClicked] = useState(false);
 
